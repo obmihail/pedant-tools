4b0c6c44f671a15ea4fb6e150fab95bb2b533a3e
oblozhikhinma <obmihail@gmail.com>
2014-12-20 18:28:18 +0500

    Merge new features without crossplatform files. Remove bottle and selenium from git in this branch. Add bottle and selenium to install scripts

--- linux-install.sh
+++ linux-install.sh
@@ -2,6 +2,13 @@
 CWD=$(pwd)
 #install selenium
 pip install selenium -t $CWD/lib/python
+#install bottle
+pip install bottle -t $CWD/lib/python/bottle
+#i don't know why bottle __init__.py is missing
+if [ ! -f lib/python/bottle/__init__.py ]; then
+    touch lib/python/bottle/__init__.py
+fi
+
 #create aliases
 echo "alias pedant-screens='python $CWD/bin/pedant-screens'" >> ~/.bashrc
 echo "alias pedant-crawler='python $CWD/bin/pedant-crawler'" >> ~/.bashrc
